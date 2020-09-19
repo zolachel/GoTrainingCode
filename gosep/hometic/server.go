694e313cf47555b9ffacb6c53b4736a06c5fe7aa
Adisorn Sangangsumalee <adisorn.sa@kbtg.tech>
2020-09-19 13:38:55 +0700

    add test for PairDeviceHandler

--- gosep/hometic/server.go
+++ gosep/hometic/server.go
@@ -9,13 +9,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//PairDeviceHandler ...
+func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"status":"active"}`))
+}
+
 func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"status":"active"}`))
-	}).Methods(http.MethodPost)
+	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
 
 	server := http.Server{
 		Addr:    "127.0.0.1:2009",
